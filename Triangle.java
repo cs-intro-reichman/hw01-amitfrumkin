6c7df368e6ec614d65580f2d2e0952590079fbac
Amitfrumkin <amit.frumkin@post.runi.ac.il>
2023-12-24 15:19:22 +0200

    add awosome code for homework 1

--- Triangle.java
+++ Triangle.java
@@ -1,7 +1,7 @@
 /*
  * Three sides can form a triangle if the sum of the lengths of any two sides is greater than the length of the remaining side. 
  * This is known as the Triangle Inequality Theorem. 
- * Write a program that tests if three given integers form a triangle.
+ * Write a program that tests if three given integers form a triangleaa.
  */ 
 public class Triangle {
 	public static void main(String[] args) {
